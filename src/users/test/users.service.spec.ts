af79077a79bae47e9792f5b092fecb1bb75f2658
Zain Ul Hassan <zain.hassan1212@gmail.com>
2021-11-11 22:41:18 +0000

    update/added-nestjs

---
+++ src/users/test/users.service.spec.ts
@@ -0,0 +1,6 @@
+describe('UsersService', () => {
+  it('should expect output to be 2', () => {
+    const result = 1 + 1;
+    expect(result).toEqual(2);
+  });
+});
---
+++ src/users/types/user.type.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  status: boolean;
+  role: string;
+  resetPasswordToken: string;
+  resetPasswordExpiry: any;
+  date: any;
+}
+
+export interface LoggedUser {
+  accessToken: string;
+  tokenType: string;
+  role: string;
+}
