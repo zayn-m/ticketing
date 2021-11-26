9b547c73d238830ed86ca307d40f8c9837102c54
Zain Ul Hassan <zain.hassan1212@gmail.com>
2021-11-26 18:42:54 +0000

    test commit

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Server running on port '+port);
+  console.log('Server is running on port '+port);
 })
 
 module.exports = app;
